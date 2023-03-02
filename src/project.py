9d2cd57c43c38a9aafadeb12b834d7c64f2dcac6
Argenna, Spencer M <spencer.m.argenna@lmco.com>
2023-03-02 16:07:16 -0500

    added several machine learning classifiers and an oversampling function for uniform class balancing

--- src/project.py
+++ src/project.py
@@ -3,8 +3,13 @@ import time
 from collections import Counter
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import SGDClassifier as SGD
 from sklearn.svm import SVC
+from sklearn.naive_bayes import MultinomialNB as MNB
+from sklearn.naive_bayes import ComplementNB as CNB
+from sklearn.ensemble import RandomForestClassifier as RF
+from sklearn.tree import DecisionTreeClassifier as DT
+from sklearn.neural_network import MLPClassifier as MLP
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_extraction.text import CountVectorizer
@@ -59,16 +64,50 @@ class my_model:
         oversample_index.extend(random_sampling_index)
 
         return oversample_index
+    
+    def multi_class_oversample(self, y, ratio=1, replace=False):
+        # ratio how many samples of smaller class per sample of bigger class
+
+        class_counts = Counter(y)
+        oversample_index = []
+        for class_label in class_counts.keys():
+            min_class = class_label
+            max_class = max(class_counts, key=class_counts.get)
+            max_class_value = class_counts[max(class_counts, key=class_counts.get)]
+            min_class_value = class_counts[min_class]
+            desired_samples_oversampled = np.floor(max_class_value * ratio)
+            number_of_times_duplicate = int(
+                np.floor(desired_samples_oversampled / min_class_value)
+            )
+            samples_remaining_required = int(desired_samples_oversampled % min_class_value)
+            # oversample_index.extend(list(np.where(y == max_class)[0]))
+            for i in range(number_of_times_duplicate):
+                oversample_index.extend(list(np.where(y == min_class)[0]))
+
+            random_sampling_index = list(
+                np.random.choice(
+                    list(np.where(y == min_class)[0]),
+                    samples_remaining_required,
+                    replace=replace,
+                )
+            )
+            oversample_index.extend(random_sampling_index)
+
+        return oversample_index
 
     def fit(self, X, y):
         # do not exceed 29 mins
+        X.reset_index(drop=True, inplace=True)
+        y.reset_index(drop=True, inplace=True)
 
         ########################
-        # might try this to make the 4 classes equal
-        # oversample_index = self.binary_oversample(y, ratio = 1)
-        # X_sample = X.loc[oversample_index, :]
-        # y_sample = y[oversample_index].to_numpy()
+        # might try this to make the 4 classes equal with over sampling
+        # oversample_index = self.multi_class_oversample(y, ratio = 1)
+        # X_sample = X.loc[oversample_index, :].reset_index(drop=True)
+        # y_sample = y[oversample_index].reset_index(drop=True).to_numpy()
 
+
+        #If we don't want over sampling use these
         X_sample = X
         y_sample = y
 
@@ -76,22 +115,26 @@ class my_model:
         wnl = WordNetLemmatizer()
 
         # Set max_dictionary_edit_distance to avoid spelling correction
-        sym_spell = SymSpell(max_dictionary_edit_distance=0, prefix_length=7)
+        sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
         dictionary_path = pkg_resources.resource_filename("symspellpy", "frequency_dictionary_en_82_765.txt")
         sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
 
         tokenizer = lambda x: tokenize(x, lowercase=True)  # gensim tokenizer
 
-        # strip tags first, then punctiation, then non alphanum, then remove white space
+        
         description = list(X_sample["Feature Request Text"])
+
+        # strip tags first, then punctiation, then non alphanum, then remove white space
         description = [preprocessing.strip_tags(d) for d in description]
+        description = [preprocessing.strip_numeric(d) for d in description]
         description = [preprocessing.strip_punctuation(d) for d in description]
         description = [preprocessing.remove_stopwords(d) for d in description]
         description = [preprocessing.strip_non_alphanum(d) for d in description]
         description = [preprocessing.strip_multiple_whitespaces(d) for d in description]
-        description = [preprocessing.strip_numeric(d) for d in description]
         description = [preprocessing.split_alphanum(d) for d in description]
+        print('Performing spelling corrections. This is an intensive step')
         description = [sym_spell.word_segmentation(d).corrected_string if len(d) > 0 else d for d in description]
+        print('Spelling corrections completed')
         description = [wnl.lemmatize(d) for d in description]
         description = [preprocessing.stem_text(d) for d in description]
 
@@ -140,12 +183,19 @@ class my_model:
         #        'gamma': space.Real(.0001,1)}
 
         # print('running Baysian Search')
-        # optimal_params = BayesSearchCV(SVC(), param_grid, cv=2, scoring='f1_micro', verbose=0)
+        # optimal_params = BayesSearchCV(SVC(class_weight = 'balanced'), param_grid, cv=2, scoring='f1_macro', verbose=0)
         # optimal_params.fit(np.asarray(XX), y_sample)
         # print(optimal_params.best_params_)
 
-        self.clf = SVC(C=1,gamma=0.3077, class_weight = 'balanced').fit(np.asarray(XX), y_sample)
-        # self.clf = SVC(C=1,gamma=0.3077).fit(np.asarray(XX), y_sample)
+        # self.clf = SVC(C=5.435,gamma=0.2884).fit(np.asarray(XX), y_sample)
+        # self.clf = CNB().fit(np.asarray(XX), y_sample)
+        # self.clf = MNB().fit(np.asarray(XX), y_sample)
+        # self.clf = RF().fit(np.asarray(XX), y_sample)
+        # self.clf = DT().fit(np.asarray(XX), y_sample)
+        # self.clf = SGD().fit(np.asarray(XX), y_sample)
+        self.clf = MLP().fit(np.asarray(XX), y_sample)
+
+
         
         return
 
@@ -156,21 +206,25 @@ class my_model:
         wnl = WordNetLemmatizer()
 
         # Set max_dictionary_edit_distance to avoid spelling correction
-        sym_spell = SymSpell(max_dictionary_edit_distance=0, prefix_length=7)
+        sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
         dictionary_path = pkg_resources.resource_filename("symspellpy", "frequency_dictionary_en_82_765.txt")
         sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
 
 
-        # strip tags first, the punctiation, then non alphanum, then remove white space
+        
         description = list(X_sample["Feature Request Text"])
+
+        # strip tags first, the punctiation, then non alphanum, then remove white space
         description = [preprocessing.strip_tags(d) for d in description]
+        description = [preprocessing.strip_numeric(d) for d in description]
         description = [preprocessing.strip_punctuation(d) for d in description]
         description = [preprocessing.remove_stopwords(d) for d in description]
         description = [preprocessing.strip_non_alphanum(d) for d in description]
         description = [preprocessing.strip_multiple_whitespaces(d) for d in description]
-        description = [preprocessing.strip_numeric(d) for d in description]
         description = [preprocessing.split_alphanum(d) for d in description]
+        print('Performing spelling corrections for predictions. This is an intensive step')
         description = [sym_spell.word_segmentation(d).corrected_string if len(d) > 0 else d for d in description]
+        print('Spelling corrections completed')
         description = [wnl.lemmatize(d) for d in description]
         description = [preprocessing.stem_text(d) for d in description]
 
